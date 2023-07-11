8da113337734b53a02efc7a92e20893b843cf34a
Stefan Baumgartner <sbaumg@gmail.com>
2023-07-11 13:00:51 +0200

    proper main

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,19 @@
-use axum::{
-    body::Body,
-    http::{Request, StatusCode},
-};
+use std::net::SocketAddr;
 
-use tower::Service;
-use webservice_rust_workshop::{router, SharedState}; // for `call`
+use webservice_rust_workshop::{router, SharedState};
 
-#[tokio::test]
-async fn hello_world() {
+type BoxError = Box<dyn std::error::Error>;
+
+#[tokio::main]
+async fn main() -> Result<(), BoxError> {
     let state = SharedState::default();
-    let mut app = router(&state);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    let response = app
-        .call(Request::builder().uri("/").body(Body::empty()).unwrap())
-        .await
-        .unwrap();
+    let app = router(&state);
 
-    assert_eq!(response.status(), StatusCode::OK);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
 
-    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-    assert_eq!(&body[..], b"<h1>Hello Axum</h1>");
-}
-
-#[ignore]
-#[tokio::test]
-async fn say_hi_unknown() {
-    let state = SharedState::default();
-    let mut app = router(&state);
-
-    let response = app
-        .call(
-            Request::builder()
-                .uri("/hello")
-                .body(Body::empty())
-                .unwrap(),
-        )
-        .await
-        .unwrap();
-
-    assert_eq!(response.status(), StatusCode::OK);
-
-    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-    assert_eq!(&body[..], b"<h1>Hello Unknown Visitor</h1>");
-}
-
-#[ignore]
-#[tokio::test]
-async fn say_hi_stefan() {
-    let state = SharedState::default();
-    let mut app = router(&state);
-
-    // `Router` implements `tower::Service<Request<Body>>` so we can
-    // call it like any tower service, no need to run an HTTP server.
-    let response = app
-        .call(
-            Request::builder()
-                .uri("/hello?name=Stefan")
-                .body(Body::empty())
-                .unwrap(),
-        )
-        .await
-        .unwrap();
-
-    assert_eq!(response.status(), StatusCode::OK);
-
-    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-    assert_eq!(&body[..], b"<h1>Hello Stefan</h1>");
+    Ok(())
 }
