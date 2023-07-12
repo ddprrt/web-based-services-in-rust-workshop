52931787e79295a3ca17c9ab10516ceabfef63de
Stefan Baumgartner <sbaumg@gmail.com>
2023-07-12 15:24:41 +0200

    step wed

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ use std::{
     sync::{Arc, RwLock},
 };
 
-use axum::{body::Bytes, Router};
+use axum::{body::Bytes, extract::Query, response::Html, routing::get, Router};
+
+use serde::Deserialize;
 
 /// Custom type for a shared state
 pub type SharedState = Arc<RwLock<AppState>>;
@@ -12,6 +14,26 @@ pub struct AppState {
     db: HashMap<String, Bytes>,
 }
 
-pub fn router(_state: &SharedState) -> Router<SharedState> {
-    todo!()
+pub fn router(state: &SharedState) -> Router<SharedState> {
+    Router::with_state(Arc::clone(state))
+        .route("/", get(hello_axum))
+        .route("/hello", get(handler_hello))
 }
+
+#[derive(Deserialize)]
+struct VisitorParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(visitor_params): Query<VisitorParams>) -> Html<String> {
+    Html(format!(
+        "<h1>Hello {}</h1>",
+        visitor_params.name.unwrap_or("Unknown Visitor".to_owned())
+    ))
+}
+
+async fn hello_axum() -> &'static str {
+    "<h1>Hello Axum</h1>"
+}
+
+async fn handler_kv_get(State(state): State<SharedState>) {}
